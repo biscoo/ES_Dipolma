8583f05cf245375fedd59c0cc308d182c339e0d1
biscoo <eng.beshoy.91@outlook.com>
2026-08-29 01:01:54 +0200

    Solve String Exercises in one app main.c
    Added functionality to count character frequency, calculate string length, and reverse the string.

--- Unit-2/Homework-2/String/EX1/main.c
+++ Unit-2/Homework-2/String/EX1/main.c
@@ -1,18 +1,17 @@
 /*
- * test.c
+ * main.c
  *
  *  Created on: Aug 28, 2026
  *      Author: Beshoy
  */
 
 #include<stdio.h>
-#include<string.h>
 
 void main() {
 
-	int i, n=0;
+	int i, n = 0, str_length = 0;
 	char c;
-	char text[100];
+	char text[100], rev[100];
 	printf("String Char Frequency Search (Linear)\n"
 			"#####################################\n");
 	printf("Enter a String: ");
@@ -23,12 +22,22 @@ void main() {
 	printf("Enter a char to be found: ");
 	fflush(stdin);
 	fflush(stdout);
-	scanf("%c",&c);
-
-	for(i=0;i<strlen(text);i++){
-		if(text[i]==c)
-			n+=1;
+	scanf("%c", &c);
+	for (i=0;;i++){
+		if(text[i] !='\0')
+			str_length+=1;
+		else
+			break;
 	}
-	printf("Frequency of the char %c: %d", c,n);
+	for (i = 0; i < str_length; i++) {
+		if (text[i] == c)
+			n += 1;
+		rev[i]=text[str_length-1-i];
+	}
+	rev[str_length]='\0';
+	printf("Frequency of the char %c: %d\n", c, n);
+	printf("The string length: %d\n", str_length);
+	printf("The reverse of the string: %s\n", rev);
+
 }
 
